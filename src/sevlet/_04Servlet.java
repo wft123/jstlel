7907cc108623b1138f4fd888f2b60d7c0cc3e7e4
kickscar <kickscar@gmail.com>
2015-12-09 05:12:31 +0900

    init commit

---
+++ src/sevlet/_04Servlet.java
@@ -0,0 +1,25 @@
+package sevlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import vo.MemberVo;
+
+@WebServlet("/04")
+public class _04Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost( request, response );
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher( "/view/04.jsp" ).forward( request, response );
+	}
+}
